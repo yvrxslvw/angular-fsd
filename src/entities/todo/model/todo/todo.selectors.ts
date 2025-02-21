5369e6fb556e95b195c5e49e24e2b99b79d349b4
yvrxslvw <yvrxslvv@gmail.com>
2025-02-21 19:01:47 +0300

    refactor(todo/model): remake selectors

--- src/entities/todo/model/todo/todo.selectors.ts
+++ src/entities/todo/model/todo/todo.selectors.ts
@@ -1,12 +1,6 @@
 import { createSelector } from '@ngrx/store';
 import { AppStore } from '@shared/interfaces';
 
-const selectTodosFeature = (store: AppStore) => store.todo.todos;
-const selectTodosIsLoadingFeature = (store: AppStore) => store.todo.isLoading;
-const selectTodosErrorFeature = (store: AppStore) => store.todo.error;
+const selectTodoFeature = (store: AppStore) => store.todo;
 
-export const selectTodos = createSelector({
-	todos: selectTodosFeature,
-	isLoading: selectTodosIsLoadingFeature,
-	error: selectTodosErrorFeature,
-});
+export const selectTodos = createSelector(selectTodoFeature, (state) => state);
