b3e013c4163028ebf0714673c98015ad1dc3ba43
yvrxslvw <yvrxslvv@gmail.com>
2025-02-27 23:56:30 +0300

    refactor(user repository): reorder methods

--- lib/backend/src/application/users/repositories/user.repository.ts
+++ lib/backend/src/application/users/repositories/user.repository.ts
@@ -4,6 +4,30 @@ import { SortDirection } from '@shared/enums';
 import { ICrudRepository } from '@shared/interfaces';
 
 export class UserRepository extends PrismaClient implements ICrudRepository<UserEntity> {
+	public async create(login: string, password: string): Promise<UserEntity> {
+		return this.user.create({
+			data: {
+				login,
+				password,
+			},
+			select: {
+				id: true,
+				login: true,
+				posts: {
+					select: {
+						id: true,
+						title: true,
+						content: true,
+						createdAt: true,
+						updatedAt: true,
+					},
+				},
+				createdAt: true,
+				updatedAt: true,
+			},
+		});
+	}
+
 	public async getAll(
 		offset?: number,
 		limit?: number,
@@ -62,30 +86,6 @@ export class UserRepository extends PrismaClient implements ICrudRepository<User
 	public async getOneByLogin(login: string): Promise<UserEntity | null> {
 		return this.user.findUnique({
 			where: { login },
-			select: {
-				id: true,
-				login: true,
-				posts: {
-					select: {
-						id: true,
-						title: true,
-						content: true,
-						createdAt: true,
-						updatedAt: true,
-					},
-				},
-				createdAt: true,
-				updatedAt: true,
-			},
-		});
-	}
-
-	public async create(login: string, password: string): Promise<UserEntity> {
-		return this.user.create({
-			data: {
-				login,
-				password,
-			},
 			select: {
 				id: true,
 				login: true,
