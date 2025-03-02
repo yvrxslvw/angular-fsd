670a8340fc86516d43fd2fa7b42d3d444178e628
yvrxslvw <yvrxslvv@gmail.com>
2025-03-02 18:38:07 +0300

    feat(auth): register

--- lib/backend/src/application/auth/commands/register.handler.ts
+++ lib/backend/src/application/auth/commands/register.handler.ts
@@ -1,9 +1,21 @@
+import { HttpStatus } from '@nestjs/common';
+import { CommandBus, CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { JwtService } from '@nestjs/jwt';
 import { RegisterCommand } from '@domains/auth';
-import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { CreateUserCommand, UserEntity } from '@domains/user';
+import { setTokens } from '@shared/utils';
 
 @CommandHandler(RegisterCommand)
 export class RegisterHandler implements ICommandHandler<RegisterCommand> {
+	constructor(
+		private readonly commandBus: CommandBus,
+		private readonly jwtService: JwtService,
+	) {}
+
 	public async execute(command: RegisterCommand) {
-		throw new Error('Method not implemented.');
+		const { login, password, response } = command;
+		const user: UserEntity = await this.commandBus.execute(new CreateUserCommand(login, password));
+		await setTokens(user, response, this.jwtService);
+		return response.status(HttpStatus.CREATED).json(user);
 	}
 }
