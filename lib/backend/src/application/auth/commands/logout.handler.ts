4f3f656b8de33a11b8fb2a37813d09b7ee421d67
yvrxslvw <yvrxslvv@gmail.com>
2025-03-02 19:02:31 +0300

    feat(auth): logout

--- lib/backend/src/application/auth/commands/logout.handler.ts
+++ lib/backend/src/application/auth/commands/logout.handler.ts
@@ -1,9 +1,15 @@
-import { LogoutCommand } from '@domains/auth';
+/* eslint @typescript-eslint/require-await: off */
+
+import { HttpStatus } from '@nestjs/common';
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { LogoutCommand } from '@domains/auth';
+import { clearTokens } from '@shared/utils';
 
 @CommandHandler(LogoutCommand)
 export class LogoutHandler implements ICommandHandler<LogoutCommand> {
 	public async execute(command: LogoutCommand) {
-		throw new Error('Method not implemented.');
+		const { response } = command;
+		clearTokens(response);
+		return response.sendStatus(HttpStatus.NO_CONTENT);
 	}
 }
