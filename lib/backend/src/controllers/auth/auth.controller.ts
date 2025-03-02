b2069aee25dd0c5113cc6a22bac708e2e254ca80
yvrxslvw <yvrxslvv@gmail.com>
2025-03-02 17:23:59 +0300

    chore(auth controller): implement command bus

--- lib/backend/src/controllers/auth/auth.controller.ts
+++ lib/backend/src/controllers/auth/auth.controller.ts
@@ -1,38 +1,44 @@
-import { Body, Controller, NotImplementedException, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Post, Req, Res, UseGuards } from '@nestjs/common';
 import { CommandBus } from '@nestjs/cqrs';
 import { ApiCookieAuth, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Request, Response } from 'express';
+import { LoginCommand, LogoutCommand, RefreshCommand, RegisterCommand } from '@domains/auth';
 import { UserEntity } from '@domains/user';
 import { AuthGuard } from '@shared/guards';
+import { extractTokens } from '@shared/utils';
 import { LoginDto, RegisterDto } from './dto';
 
 @ApiTags('Авторизация')
 @Controller('auth')
 export class AuthController {
-	constructor(commandBus: CommandBus) {}
+	constructor(private readonly commandBus: CommandBus) {}
 
 	@ApiOperation({ summary: 'Авторизация' })
 	@ApiResponse({ status: 201, description: 'Успешная авторизация', type: UserEntity })
 	@ApiResponse({ status: 400, description: 'Ошибка валидации' })
 	@ApiResponse({ status: 403, description: 'Неверный логин или пароль' })
 	@Post('login')
-	public async login(@Body() loginDto: LoginDto) {
-		throw new NotImplementedException();
+	public async login(@Res() response: Response, @Body() loginDto: LoginDto) {
+		const { login, password, rememberMe } = loginDto;
+		return this.commandBus.execute(new LoginCommand(login, password, rememberMe, response));
 	}
 
 	@ApiOperation({ summary: 'Регистрация' })
 	@ApiResponse({ status: 201, description: 'Успешная регистрация', type: UserEntity })
 	@ApiResponse({ status: 400, description: 'Ошибка валидации или пользователь уже существует' })
 	@Post('register')
-	public async register(@Body() registerDto: RegisterDto) {
-		throw new NotImplementedException();
+	public async register(@Res() response: Response, @Body() registerDto: RegisterDto) {
+		const { login, password } = registerDto;
+		return this.commandBus.execute(new RegisterCommand(login, password, response));
 	}
 
 	@ApiOperation({ summary: 'Обновление токенов' })
 	@ApiResponse({ status: 201, description: 'Успешное обновление токенов', type: UserEntity })
 	@ApiResponse({ status: 403, description: 'Нет токена обновления или токен обновления недействителен' })
 	@Post('refresh')
-	public async refresh() {
-		throw new NotImplementedException();
+	public async refresh(@Req() request: Request, @Res() response: Response) {
+		const { refreshToken } = extractTokens(request);
+		return this.commandBus.execute(new RefreshCommand(refreshToken, response));
 	}
 
 	@ApiOperation({ summary: 'Выход из аккаунта' })
@@ -41,7 +47,7 @@ export class AuthController {
 	@ApiCookieAuth()
 	@UseGuards(AuthGuard)
 	@Post('logout')
-	public async logout() {
-		throw new NotImplementedException();
+	public async logout(@Res() response: Response) {
+		return this.commandBus.execute(new LogoutCommand(response));
 	}
 }
