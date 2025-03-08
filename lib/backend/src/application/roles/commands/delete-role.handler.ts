f19721e15d39dd7ffd070c19a07d6b772025e7eb
yvrxslvw <yvrxslvv@gmail.com>
2025-03-08 18:48:50 +0300

    feat(roles): deleting role

---
+++ lib/backend/src/application/roles/commands/delete-role.handler.ts
@@ -0,0 +1,17 @@
+import { RoleRepository } from '@application/roles/repositories';
+import { DeleteRoleCommand, RoleEntity } from '@domains/role';
+import { HttpStatus } from '@nestjs/common';
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { BackendException } from '@shared/exceptions';
+
+@CommandHandler(DeleteRoleCommand)
+export class DeleteRoleHandler implements ICommandHandler<DeleteRoleCommand> {
+	constructor(private readonly roleRepo: RoleRepository) {}
+
+	public async execute(command: DeleteRoleCommand): Promise<RoleEntity> {
+		const { id } = command;
+		const role = await this.roleRepo.getOneById(id);
+		if (!role) throw new BackendException(HttpStatus.NOT_FOUND, `Роли с ID ${id} не существует`);
+		return this.roleRepo.delete(id);
+	}
+}
--- lib/backend/src/application/roles/commands/index.ts
+++ lib/backend/src/application/roles/commands/index.ts
@@ -1,4 +1,5 @@
 import { CreateRoleHandler } from './create-role.handler';
+import { DeleteRoleHandler } from './delete-role.handler';
 import { UpdateRoleHandler } from './update-role.handler';
 
-export const CommandHandlers = [CreateRoleHandler, UpdateRoleHandler];
+export const CommandHandlers = [CreateRoleHandler, UpdateRoleHandler, DeleteRoleHandler];
