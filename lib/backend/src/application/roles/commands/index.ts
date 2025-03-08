9089479b44fb7a7925825a9f8570b6867635d4c7
yvrxslvw <yvrxslvv@gmail.com>
2025-03-08 18:46:32 +0300

    feat(roles): updating role

--- lib/backend/src/application/roles/commands/index.ts
+++ lib/backend/src/application/roles/commands/index.ts
@@ -1,3 +1,4 @@
 import { CreateRoleHandler } from './create-role.handler';
+import { UpdateRoleHandler } from './update-role.handler';
 
-export const CommandHandlers = [CreateRoleHandler];
+export const CommandHandlers = [CreateRoleHandler, UpdateRoleHandler];
---
+++ lib/backend/src/application/roles/commands/update-role.handler.ts
@@ -0,0 +1,21 @@
+import { RoleRepository } from '@application/roles/repositories';
+import { RoleEntity, UpdateRoleCommand } from '@domains/role';
+import { HttpStatus } from '@nestjs/common';
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { BackendException } from '@shared/exceptions';
+
+@CommandHandler(UpdateRoleCommand)
+export class UpdateRoleHandler implements ICommandHandler<UpdateRoleCommand> {
+	constructor(private readonly roleRepo: RoleRepository) {}
+
+	public async execute(command: UpdateRoleCommand): Promise<RoleEntity> {
+		const { id, tag, name } = command;
+		const role = await this.roleRepo.getOneById(id);
+		if (!role) throw new BackendException(HttpStatus.NOT_FOUND, `Роли с ID ${id} не существует`);
+		if (tag) {
+			const candidate = await this.roleRepo.getOneByTag(tag);
+			if (candidate) throw new BackendException(HttpStatus.BAD_REQUEST, `Роль с тегом ${tag} уже существует`);
+		}
+		return this.roleRepo.update(id, tag, name);
+	}
+}
