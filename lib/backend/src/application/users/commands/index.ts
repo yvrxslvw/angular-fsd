d6fcdc1532856994882d1740a48593eab03b9b2e
yvrxslvw <yvrxslvv@gmail.com>
2025-03-08 22:57:44 +0300

    feat(users): removing role from user

--- lib/backend/src/application/users/commands/index.ts
+++ lib/backend/src/application/users/commands/index.ts
@@ -1,6 +1,13 @@
 import { AddUserRoleHandler } from './add-user-role.handler';
 import { CreateUserHandler } from './create-user.handler';
 import { DeleteUserHandler } from './delete-user.handler';
+import { RemoveUserRoleHandler } from './remove-user-role.handler';
 import { UpdateUserHandler } from './update-user.handler';
 
-export const CommandHandlers = [CreateUserHandler, UpdateUserHandler, DeleteUserHandler, AddUserRoleHandler];
+export const CommandHandlers = [
+	CreateUserHandler,
+	UpdateUserHandler,
+	DeleteUserHandler,
+	AddUserRoleHandler,
+	RemoveUserRoleHandler,
+];
---
+++ lib/backend/src/application/users/commands/remove-user-role.handler.ts
@@ -0,0 +1,21 @@
+import { GetOneUserQuery, RemoveUserRoleCommand, UserEntity } from '@domains/user';
+import { HttpStatus } from '@nestjs/common';
+import { CommandHandler, ICommandHandler, QueryBus } from '@nestjs/cqrs';
+import { BackendException } from '@shared/exceptions';
+import { UserRepository } from '../repositories';
+
+@CommandHandler(RemoveUserRoleCommand)
+export class RemoveUserRoleHandler implements ICommandHandler<RemoveUserRoleCommand> {
+	constructor(
+		private readonly queryBus: QueryBus,
+		private readonly userRepo: UserRepository,
+	) {}
+
+	public async execute(command: RemoveUserRoleCommand): Promise<UserEntity> {
+		const { id, roleId } = command;
+		const user: UserEntity = await this.queryBus.execute(new GetOneUserQuery(id));
+		if (!user.roles.some((role) => role.id === roleId))
+			throw new BackendException(HttpStatus.BAD_REQUEST, 'У пользователя нет данной роли');
+		return this.userRepo.removeRole(id, roleId);
+	}
+}
